439678fde0ce95377a7079f91802f35db8fe83bb
jordn24 <45545394+jordn24@users.noreply.github.com>
2023-06-13 20:16:12 +1000

    Main Parent Code

---
+++ index.js
@@ -0,0 +1,22 @@
+const { spawn } = require('child_process');
+const fs = require('fs');
+
+// Read the active bot paths from the JSON file
+const activeBotsData = fs.readFileSync('active-bots.json');
+const activeBots = JSON.parse(activeBotsData).activeBots;
+
+// Function to run each bot
+function runBot(botDir) {
+  const botProcess = spawn('node', [`${botDir}/bot${botDir}.js`]);
+
+  botProcess.stdout.on('data', (data) => {
+    console.log(`[${botDir}] ${data}`);
+  });
+
+  botProcess.stderr.on('data', (data) => {
+    console.error(`[${botDir}] ${data}`);
+  });
+}
+
+// Run each bot
+botDirectories.forEach(runBot);
