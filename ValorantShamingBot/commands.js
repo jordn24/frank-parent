fe3c638f38ef24fd6b34189cad63d8ddc309f33f
jordn24 <45545394+jordn24@users.noreply.github.com>
2023-06-25 02:49:30 +1000

    Added third button to sort by percentages

--- ValorantShamingBot/commands.js
+++ ValorantShamingBot/commands.js
@@ -9,11 +9,34 @@ const client = new Client({
     intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] 
   });
 
+
+function sortFields(fields, sort) {
+    let sortedFields;
+    let index = 1;
+
+    if(sort == "number"){
+        sortedFields = fields.sort((fieldA, fieldB) => (fieldA.raw_value < fieldB.raw_value) ? 1 : -1);
+    } else if (sort == "percentage"){
+        index = 1;
+        sortedFields = fields.sort((fieldA, fieldB) => (fieldA.rawPercentage < fieldB.rawPercentage) ? 1 : -1);
+    }
+
+    // Add placement number in front
+    for(var i = 0; i < sortedFields.length; i++){
+        sortedFields[i].name = sortedFields[i].name.replace(/\d+\.\s/, '')
+        sortedFields[i].name = index + ". " + sortedFields[i].name
+        index = index + 1
+    }
+
+    return sortedFields;
+}
+
+
 client.on('messageCreate', async message => {
     if (message.content.toLowerCase() == '!newscoreboard'){
 
         // Create buttons for options
-        let option1Button = new MessageButton()
+        const option1Button = new MessageButton()
         .setCustomId('current')
         .setLabel('This Act')
         .setStyle('PRIMARY');
@@ -23,9 +46,14 @@ client.on('messageCreate', async message => {
         .setLabel('All Time')
         .setStyle('DANGER');
 
+        const option3Button = new MessageButton()
+        .setCustomId('percentage')
+        .setLabel('Percentage')
+        .setStyle('SECONDARY');
+
         // Interactive Embed and Button Row 
         const buttonRow = new MessageActionRow()
-        .addComponents(option1Button, option2Button);
+        .addComponents(option1Button, option2Button, option3Button);
 
         const embed = new MessageEmbed();
 
@@ -43,25 +71,14 @@ client.on('messageCreate', async message => {
         // Add data from database to fields
         users.forEach((user) => {
             actFields.push({name: user.user + ":", value: "Has bottom fragged " + user.score + " time/s\nWith a rate of "
-            + parseInt(user.percentage) + "%", raw_value: parseInt(user.score)})
+            + parseFloat(user.percentage).toFixed(2) + "%", raw_value: parseInt(user.score), rawPercentage: parseFloat(user.percentage)})
             allTimeFields.push({name: user.user + ":", value: "Has bottom fragged " + user.all_time_score + " time/s\nWith a rate of "
-                + parseInt(user.all_time_percentage) + "%", raw_value: parseInt(user.all_time_score)})
+                + parseFloat(user.all_time_percentage).toFixed(2) + "%", raw_value: parseInt(user.all_time_score), rawPercentage: parseFloat(user.all_time_percentage)})
         })
-
-        // Sort fields
-        actFields.sort((fieldA, fieldB) => (fieldA.raw_value < fieldB.raw_value) ? 1 : -1)
-        allTimeFields.sort((fieldA, fieldB) => (fieldA.raw_value < fieldB.raw_value) ? 1 : -1)
-
-        // Add placement number in front
-        for(var i = 0; i < actFields.length; i++){
-            actFields[i].name = index + ". " + actFields[i].name
-            allTimeFields[i].name = index + ". " + allTimeFields[i].name
-            index = index + 1
-        }
         
         // Default Setting for Leaderboard
         embed.setTitle("Act Leaderboard");
-        embed.setFields(actFields);
+        embed.setFields(sortFields(actFields, "number"));
         embed.setColor('BLUE');
 
         // Send the embed with the buttons as a message
@@ -79,13 +96,24 @@ client.on('messageCreate', async message => {
                 switch (clickedOption) {
                 case 'current':
                     embed.setTitle("Act Leaderboard");
-                    embed.setFields(actFields);
+                    embed.setFields(sortFields(actFields, "number"));
                     embed.setColor('BLUE');
                     break;
                 case 'alltime':
                     embed.setTitle("All Time Leaderboard");
-                    embed.setFields(allTimeFields);
+                    embed.setFields(sortFields(allTimeFields, "number"));
                     embed.setColor('RED');
+                    break;
+                case 'percentage':
+                    if (embed.title === "Act Leaderboard") {
+                        embed.setTitle("Act Leaderboard (Sorted by Percentage)");
+                        embed.setFields(sortFields(actFields, "percentage"));
+                        embed.setColor('GREY');
+                    } else if (embed.title === "All Time Leaderboard") {
+                        embed.setTitle("All Time Leaderboard (Sorted by Percentage)");
+                        embed.setFields(sortFields(allTimeFields, "percentage"));
+                        embed.setColor('GREY');
+                    }
                     break;
                 }
     
