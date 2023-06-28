6145bf57034b648ac8b2e01d9dcfe51dd97cb4a9
jordn24 <45545394+jordn24@users.noreply.github.com>
2023-06-28 20:19:34 +1000

    Fixed some bugs

--- ValorantShamingBot/index.js
+++ ValorantShamingBot/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require('dotenv').config({ path: '../.env' });
 
 const { Client, Intents} = require('discord.js');
 
@@ -11,7 +11,8 @@ const client = new Client({
   });
 
 const token = process.env.BOT_TOKEN;
-const channelid = process.env.GENERAL_CHANNEL_ID;
+// const channelid = process.env.GENERAL_CHANNEL_ID;
+const channelid = "865432456702590978"
 
 const { getLatestMatch, getPositionInMatch } = require('../Core/Valorant');
 
@@ -23,57 +24,56 @@ client.on('ready', async async => {
 
     let users = await dbHandler.getUsers();
     // For every username
-    users.forEach((user) => {
+    for (const user of users) {
+        console.log("\nStarting for: " + user.user);
         // Check latest match
-        getLatestMatch(user.user, user.tag).then((match_id) => {
-            // Get position in latest match
-            getPositionInMatch(match_id, user.user).then(async (position) => {
-		        // If position is last
-                if (position === 10 && user.latest_match !== match_id) {
-                    
-                    // Send message in general
-                    formatted_msg = "<@" + user.disc_id + "> " + messages[Math.floor(Math.random() * messages.length)]
-                    tracker_link = "https://tracker.gg/valorant/match/" + match_id + "?handle=" + encodeURIComponent(user.user) + "%23" + user.tag
+        const match_id = await getLatestMatch(user.user, user.tag);
+        console.log(match_id + " is the latest match");
+    
+        // Get position in latest match
+        const position = await getPositionInMatch(match_id, user.user);
+        console.log(user.user + " finished " + position);
 
-                    // Send to channel id in config
-                    client.channels.cache.get(channelid).send(formatted_msg)
-                    client.channels.cache.get(channelid).send(tracker_link)                                
+        // If position is last
+        if (position === 10 && user.latest_match !== match_id) { 
+            // Send message in general
+            formatted_msg = "<@" + user.disc_id + "> " + messages[Math.floor(Math.random() * messages.length)]
+            tracker_link = "https://tracker.gg/valorant/match/" + match_id + "?handle=" + encodeURIComponent(user.user) + "%23" + user.tag
 
-                    // Call Total Matches APIs
-                    let newActMatches = await APIHandler.post(process.env.WEB_TRACKERGG_API + process.env.WEB_TRACKERGG_ACT_URI + "?user=" + 
-                        user.user + "&tag=" + user.tag);
+            // Send to channel id in config
+            client.channels.cache.get(channelid).send(formatted_msg)
+            client.channels.cache.get(channelid).send(tracker_link)                                
 
-                    let newTotalMatches = await APIHandler.post(process.env.WEB_TRACKERGG_API + process.env.WEB_TRACKERGG_TOT_URI+ "?user=" + 
-                        user.user + "&tag=" + user.tag);
+            // Call Act Matches APIs
+            const newActMatches = await APIHandler.post(process.env.WEB_TRACKERGG_API + process.env.WEB_TRACKERGG_ACT_URI + "?user=" +
+                user.user + "&tag=" + user.tag);
 
-                    // Calculate new percentage
-                    let newPercentage = (( (parseInt(user.score) + 1) / parseInt(newActMatches.data)) * 100)
-                    let newAllTimePercentage = (( (parseInt(user.all_time_score) + 1) / parseInt(newTotalMatches.data)) * 100)
-                    
-                    if(user.score == 0){
-                        newPercentage = 0;
-                    }
-                    if(user.all_time_score == 0){
-                        newAllTimePercentage = 0;
-                    }
-                    
-                    // Update Scores
-                    await dbHandler.updateUser(user._id, "score", (parseInt(user.score) + 1).toString());
-                    await dbHandler.updateUser(user._id, "all_time_score", (parseInt(user.all_time_score) + 1).toString());
-                    // Update Matches
-                    await dbHandler.updateUser(user._id, "matches", newActMatches.data.toString());
-                    await dbHandler.updateUser(user._id, "alltime_matches_played", newTotalMatches.data.toString());
-                    // Update Percentage
-                    await dbHandler.updateUser(user._id, "percentage", newPercentage.toString());
-                    await dbHandler.updateUser(user._id, "all_time_percentage", newAllTimePercentage.toString());
-                    // Update latest match id
-                    await dbHandler.updateUser(user._id, "latest_match", match_id);
-                } else {
-                    console.log("Not bottom.")
-                }
-            })
-        })
-    })
+            // Calculate new percentage
+            let newPercentage;
+            if(newActMatches){
+                newPercentage = (( (parseInt(user.score) + 1) / parseInt(newActMatches.data)) * 100)
+            }
+            
+            if(user.score == 0){
+                newPercentage = 0;
+            }
+            
+            if(newPercentage && newAllTimePercentage && newActMatches.data && newTotalMatches.data && user.score && user.all_time_score && match_id){
+                // Update Scores
+                await dbHandler.updateUser(user._id, "score", (parseInt(user.score) + 1).toString());
+                // Update Matches
+                await dbHandler.updateUser(user._id, "matches", newActMatches.data.toString());
+                // Update Percentage
+                await dbHandler.updateUser(user._id, "percentage", newPercentage.toString());
+                // Update latest match id
+                await dbHandler.updateUser(user._id, "latest_match", match_id);
+            } else {
+                console.log("Something went wrong with the data gathered... NOT UPDATING DB")
+            }
+        } else {
+            console.log("Not bottom.")
+        }
+    }
     setTimeout(() => process.exit(1), 60000)
 })
 
