0293f77cc1bb3fd56c0201940f0971991be5a1ba
EC2 Default User <ec2-user@ip-172-31-18-214.ap-southeast-2.compute.internal>
2023-07-08 13:07:22 +0000

    removed unused files

--- ValorantShamingBot/start_job.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-source /home/ec2-user/frank/frank-parent/frank-parent/.env
-~/.nvm/versions/node/v16.9.1/bin/node /home/ec2-user/frank/frank-parent/frank-parent/ValorantShamingBot/index.js
